47c491485a47eaa970fe92faac6402a050cb6bbd
Fernando de Sousa Mello <fdesmello@gmail.com>
2025-02-24 05:04:08 -0300

    Introduced methods
    Separated the exoplanets by method of discovery in the visual.

--- Extracting_data_Online.py
+++ Extracting_data_Online.py
@@ -2,6 +2,7 @@ import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
 import math
+from collections import Counter
 
 # Defining the url---------------------------------------------------
 #--------------------------------------------------------------------
@@ -20,7 +21,7 @@ querytap = "/TAP/sync?query="
 
 query = input("Enter query (Type Enter to go with the default query): ")
 if len(query) < 1: #break
-    query = "select+sy_dist,glat,glon+from+pscomppars&format=json"
+    query = "select+sy_dist,glat,glon,discoverymethod+from+pscomppars&format=json"
 
 url = service_url + querytap + query
 
@@ -37,10 +38,11 @@ try:
 except:
     js = None
 
-# The data (distance and coordinates) we will keep-------------------
+# The data (distance, coordinates, technique) we will keep-------------------
 sy_dist = list()
 glat = list()
 glon = list()
+meth = list()
 
 count_exoplanets_distance = 0
 count_exoplanets_others = 0
@@ -49,6 +51,7 @@ for item in js :
         sy_dist.append(float(item["sy_dist"]))
         glat.append(float(item["glat"]))
         glon.append(float(item["glon"]))
+        meth.append(str(item["discoverymethod"]))
         count_exoplanets_distance += 1
     except :
         count_exoplanets_others += 1
@@ -57,6 +60,7 @@ for item in js :
 exoplanets = count_exoplanets_distance + count_exoplanets_others
 print(f"Number of exoplanets discovered: {exoplanets}")
 print(f"Number of exoplanets with distance determined: {count_exoplanets_distance}")
+print(Counter(meth))
 
 # Operating on data--------------------------------------------------
 #--------------------------------------------------------------------
@@ -65,6 +69,8 @@ print("Operating on data")
 X = list()
 Y = list()
 Z = list()
+M = list()
+Mset = set(meth)
 
 pctoly = 3.26156 # Convertion of parsec to light years
 Dcg = 26000 # Distance of the Solar System to center of the Galaxy in light years
@@ -74,6 +80,7 @@ for item in range(len(sy_dist)) :
     X.append(sy_dist[item]*pctoly*math.cos(math.radians(glat[item]))*math.cos(math.radians(glon[item]))-Dcg)
     Y.append(sy_dist[item]*pctoly*math.cos(math.radians(glat[item]))*math.sin(math.radians(glon[item])))
     Z.append(sy_dist[item]*pctoly*math.sin(math.radians(glat[item])))
+    M.append(meth[item])
 
 # Writing in exit file-----------------------------------------------
 #--------------------------------------------------------------------
@@ -88,8 +95,25 @@ except:
     exit()
 
 for item in range(len(sy_dist)) :
-    fout.write(str("{:.2f}".format(X[item]) + " " + str("{:.2f}".format(Y[item])) + " " + str("{:.2f}".format(Z[item])) + "\n"))
-# The "{:.2f}".format() inside the str() is to format the float to 2 decimal algarisms
+    fout.write(str("{:.3f}".format(X[item]) + " " + str("{:.3f}".format(Y[item])) + " " + str("{:.3f}".format(Z[item])) + "\n"))
+# The "{:.3f}".format() inside the str() is to format the float to 3 decimal algarisms
+
+fout.close()
+
+#--------------------------------------------------------------------
+fname = "Exoplanets_coordinates_methods.txt"
+
+print("Writing on:", fname)
+
+try:
+    fout = open(fname, "w")
+except:
+    print("File cannot be opened:", fname)
+    exit()
+
+for item in range(len(sy_dist)) :
+    fout.write(str("{:.3f}".format(X[item]) + ";" + str("{:.3f}".format(Y[item])) + ";" + str("{:.3f}".format(Z[item])) + ";" + str("{}".format(M[item])) + "\n"))
+# The "{:.3f}".format() inside the str() is to format the float to 3 decimal algarisms
 
 fout.close()
 
--- From_txt_to_json.py
+++ From_txt_to_json.py
@@ -1,5 +1,6 @@
 import json
 
+#--------------------------------------------------------------------
 # Read the text file and split it into lines
 with open("Exoplanets_coordinates.txt", "r") as f:
     lines = f.readlines()
@@ -10,3 +11,28 @@ data = [list(map(float, line.strip().split())) for line in lines]
 # Write the data to a JSON file
 with open("Exoplanets_coordinates.json", "w") as f:
     json.dump(data, f)
+
+#--------------------------------------------------------------------
+# Read the text file and split it into lines
+with open("Exoplanets_coordinates_methods.txt", "r") as f:
+    lines = f.readlines()
+
+# Convert each line to a list of coordinates with correct data types
+data2 = []
+for line in lines:
+    parts = line.strip().split(";")
+    if len(parts) == 4:  # Ensure there are four parts
+        try:
+            x = float(parts[0])
+            y = float(parts[1])
+            z = float(parts[2])
+            label = parts[3]
+            data2.append([x, y, z, label])
+        except ValueError:
+            print(f"Skipping line due to invalid numeric data: {line.strip()}") #report errors and skip.
+    else:
+        print(f"Skipping line due to incorrect number of fields: {line.strip()}") #report errors and skip.
+
+# Write the data to a JSON file
+with open("Exoplanets_coordinates_methods.json", "w") as f:
+    json.dump(data2, f, indent=4) #using indent=4 for better readability
